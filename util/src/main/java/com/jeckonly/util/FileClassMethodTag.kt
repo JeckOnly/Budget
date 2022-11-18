672bed7a2f832547abd534c3d7b62bd6d6973fb9
JeckOnly <2322802221@qq.com>
2022-11-18 23:30:57 +0800

    更新实现以适配新版Android Studio的Tag的长度限制

--- util/src/main/java/com/jeckonly/util/FileClassMethodTag.kt
+++ util/src/main/java/com/jeckonly/util/FileClassMethodTag.kt
@@ -4,6 +4,6 @@ import timber.log.Timber
 
 class FileClassMethodTag: Timber.DebugTree(){
     override fun createStackElementTag(e: StackTraceElement): String {
-        return "(${e.fileName}:${e.lineNumber})[${e.className}][${e.methodName}]"
+        return "[${e.fileName}][${e.methodName}]"
     }
 }
