877a04921db6d44a517e52b2a7511735dbf1c848
JeckOnly <2322802221@qq.com>
2022-11-20 20:41:03 +0800

    删除

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -22,4 +22,3 @@ const val sandwich_version = "1.3.0"
 const val material3_version = "1.0.0"
 const val annotation_version = "1.5.0"
 const val timber_version = "5.0.1"
-//    implementation 'com.jakewharton.timber:timber:5.0.1'
