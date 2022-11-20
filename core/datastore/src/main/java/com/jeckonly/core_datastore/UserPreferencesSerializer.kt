06e54110663a769dbadaa95032dcb14108104fce
JeckOnly <2322802221@qq.com>
2022-11-20 18:24:30 +0800

    datastore编写

--- core/datastore/src/main/java/com/jeckonly/core_datastore/UserPreferencesSerializer.kt
+++ core/datastore/src/main/java/com/jeckonly/core_datastore/UserPreferencesSerializer.kt
@@ -10,15 +10,15 @@ import java.io.OutputStream
 /**
  * 这个类似乎是固定写法？
  */
-//object UserPreferencesSerializer : Serializer<UserPreferences> {
-//    override val defaultValue: UserPreferences = UserPreferences.getDefaultInstance()
-//    override suspend fun readFrom(input: InputStream): UserPreferences {
-//        try {
-//            return UserPreferences.parseFrom(input)
-//        } catch (exception: InvalidProtocolBufferException) {
-//            throw CorruptionException("Cannot read proto.", exception)
-//        }
-//    }
-//
-//    override suspend fun writeTo(t: UserPreferences, output: OutputStream) = t.writeTo(output)
-//}
+object UserPreferencesSerializer : Serializer<UserPreferences> {
+    override val defaultValue: UserPreferences = UserPreferences.getDefaultInstance()
+    override suspend fun readFrom(input: InputStream): UserPreferences {
+        try {
+            return UserPreferences.parseFrom(input)
+        } catch (exception: InvalidProtocolBufferException) {
+            throw CorruptionException("Cannot read proto.", exception)
+        }
+    }
+
+    override suspend fun writeTo(t: UserPreferences, output: OutputStream) = t.writeTo(output)
+}
--- core/datastore/src/main/java/com/jeckonly/core_datastore/UserPrefsDataSource.kt
+++ core/datastore/src/main/java/com/jeckonly/core_datastore/UserPrefsDataSource.kt
@@ -2,31 +2,23 @@ package com.jeckonly.core_datastore
 
 import androidx.datastore.core.DataStore
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 import javax.inject.Singleton
 
-//@Singleton
-//class UserPrefsDataSource @Inject constructor(
-//    private val userPreferences: DataStore<UserPreferences>
-//) {
-//    fun getDownloadStateFlow(): Flow<Boolean> {
-//        return userPreferences.data.map {
-//           it.finishDownload
-//        }
-//    }
-//
-//    suspend fun isFinishDownload(): Boolean {
-//        return userPreferences.data.map {
-//            it.finishDownload
-//        }.first()
-//    }
-//
-//    suspend fun updateDownloadState(isFinished: Boolean) {
-//        userPreferences.updateData { preferences ->
-//            val currentState = preferences.finishDownload
-//            preferences.toBuilder().setFinishDownload(isFinished).build()
-//        }
-//    }
-//}
+@Singleton
+class UserPrefsDataSource @Inject constructor(
+    private val userPreferences: DataStore<UserPreferences>
+) {
+    fun getDatabaseInitState(): Flow<Boolean> {
+        return userPreferences.data.map {
+           it.databaseTypeHasInit
+        }
+    }
+
+    suspend fun updateDatabaseInitState(hasInit: Boolean) {
+        userPreferences.updateData { preferences ->
+            preferences.toBuilder().setDatabaseTypeHasInit(hasInit).build()
+        }
+    }
+}
