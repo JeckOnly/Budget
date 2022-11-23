1162d954d20b61ccea73937468bacbd78b6e605c
JeckOnly <2322802221@qq.com>
2022-11-23 12:38:27 +0800

    增加图标和字符串资源

--- core/designsystem/src/main/java/com/jeckonly/designsystem/theme/Type.kt
+++ core/designsystem/src/main/java/com/jeckonly/designsystem/theme/Type.kt
@@ -1,9 +1,17 @@
 package com.jeckonly.designsystem.theme
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.jeckonly.designsystem.Mdf
 
 // Material 3 typography
 val budgetTypography = Typography(
@@ -80,3 +88,34 @@ val budgetTypography = Typography(
         letterSpacing = 0.5.sp
     )
 )
+
+@Preview(showBackground = true, backgroundColor = 0xffffff, showSystemUi = true)
+@Composable
+fun PreviewTypography() {
+    Column() {
+        Text(text = "你好吗，世界, headlineLarge", style = budgetTypography.headlineLarge)
+        Spacer(modifier = Mdf.height(10.dp))
+        Text(text = "你好吗，世界, headlineMedium", style = budgetTypography.headlineMedium)
+        Spacer(modifier = Mdf.height(10.dp))
+        Text(text = "你好吗，世界, headlineSmall", style = budgetTypography.headlineSmall)
+        Spacer(modifier = Mdf.height(10.dp))
+        Text(text = "你好吗，世界, titleLarge", style = budgetTypography.titleLarge)
+        Spacer(modifier = Mdf.height(10.dp))
+        Text(text = "你好吗，世界, titleMedium", style = budgetTypography.titleMedium)
+        Spacer(modifier = Mdf.height(10.dp))
+        Text(text = "你好吗，世界, titleSmall", style = budgetTypography.titleSmall)
+        Spacer(modifier = Mdf.height(10.dp))
+        Text(text = "你好吗，世界, bodyLarge", style = budgetTypography.bodyLarge)
+        Spacer(modifier = Mdf.height(10.dp))
+        Text(text = "你好吗，世界, bodyMedium", style = budgetTypography.bodyMedium)
+        Spacer(modifier = Mdf.height(10.dp))
+        Text(text = "你好吗，世界, bodySmall", style = budgetTypography.bodySmall)
+        Spacer(modifier = Mdf.height(10.dp))
+        Text(text = "你好吗，世界, labelLarge", style = budgetTypography.labelLarge)
+        Spacer(modifier = Mdf.height(10.dp))
+        Text(text = "你好吗，世界, labelMedium", style = budgetTypography.labelMedium)
+        Spacer(modifier = Mdf.height(10.dp))
+        Text(text = "你好吗，世界, labelSmall", style = budgetTypography.labelSmall)
+        Spacer(modifier = Mdf.height(10.dp))
+    }
+}
