5d73e20a6e63983dd9074ff014c1253e11407c21
JeckOnly <2322802221@qq.com>
2022-11-23 20:10:08 +0800

    类型按钮加上动画

--- feature/chooseType/src/main/java/com/jeckonly/choosetype/ui/TypeItem.kt
+++ feature/chooseType/src/main/java/com/jeckonly/choosetype/ui/TypeItem.kt
@@ -1,7 +1,9 @@
 package com.jeckonly.choosetype.ui
 
+import androidx.compose.animation.core.Animatable
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
@@ -11,9 +13,10 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.contentColorFor
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,28 +29,52 @@ private const val ICON_SIZE = 35
 
 @Composable
 fun TypeItem(typeUI: ChooseTypeTypeUI, onClick: () -> Unit, modifier: Modifier = Modifier) {
+
+    val interactionSource = remember {
+        MutableInteractionSource()
+    }
+    var animateStartHelper by remember {
+        mutableStateOf(0)
+    }
+    val scaleAnimated = remember {
+        Animatable(1f)
+    }
+    LaunchedEffect(key1 = animateStartHelper, block = {
+        if (animateStartHelper > 0) {
+            scaleAnimated.animateTo(0.7f)
+            scaleAnimated.animateTo(1f)
+        }
+    })
     Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-        Box(
-            modifier = Mdf
-                .background(
-                    color = MaterialTheme.colorScheme.secondaryContainer.copy(alpha = 0.5f),
-                    shape = CircleShape
-                )
-                .padding(10.dp)
-                .clickable {
-                    onClick()
-                },
-            contentAlignment = Alignment.Center
-        ) {
-            Icon(
-                painter = painterResource(id = typeUI.iconId),
-                contentDescription = typeUI.typeName,
+        Box(modifier = Mdf.scale(scaleAnimated.value)) {
+            Box(
                 modifier = Mdf
-                    .size(ICON_SIZE.dp),
-                tint = contentColorFor(
-                    backgroundColor = MaterialTheme.colorScheme.secondaryContainer
+                    .clickable(
+                        interactionSource = interactionSource,
+                        indication = null,
+                        onClick = {
+                            // 通过不断改变state的true或false来开启动画
+                            animateStartHelper++
+                            onClick()
+                        }
+                    )
+                    .background(
+                        color = MaterialTheme.colorScheme.secondaryContainer.copy(alpha = 0.5f),
+                        shape = CircleShape
+                    )
+                    .padding(10.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    painter = painterResource(id = typeUI.iconId),
+                    contentDescription = typeUI.typeName,
+                    modifier = Mdf
+                        .size(ICON_SIZE.dp),
+                    tint = contentColorFor(
+                        backgroundColor = MaterialTheme.colorScheme.secondaryContainer
+                    )
                 )
-            )
+            }
         }
 
         Text(
