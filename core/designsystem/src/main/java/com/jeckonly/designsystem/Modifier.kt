7e983c9aa961dc088612c658a5130d4471f5bcd4
JeckOnly <2322802221@qq.com>
2022-11-25 13:30:34 +0800

    方便的函数

--- core/designsystem/src/main/java/com/jeckonly/designsystem/Modifier.kt
+++ core/designsystem/src/main/java/com/jeckonly/designsystem/Modifier.kt
@@ -1,5 +1,22 @@
 package com.jeckonly.designsystem
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 
 typealias Mdf = Modifier
+
+fun Modifier.noIndicationClickable(
+    enabled: Boolean = true,
+    onClick: () -> Unit
+) = composed {
+    this.clickable(
+        interactionSource = remember {
+            MutableInteractionSource()
+        },
+        indication = null,
+        onClick = onClick
+    )
+}
