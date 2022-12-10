f2ddaca76b4a417976dff732db9291fc16cc3c96
JeckOnly <2322802221@qq.com>
2022-12-11 00:01:09 +0800

    UI修改

--- feature/home/src/main/java/com/jeckonly/home/HomeScreen.kt
+++ feature/home/src/main/java/com/jeckonly/home/HomeScreen.kt
@@ -2,6 +2,7 @@ package com.jeckonly.home
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -9,6 +10,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -73,7 +75,7 @@ fun HomeScreen(
         )
         Spacer(modifier = Modifier.height(20.dp))
         LazyColumn(
-            modifier = Mdf.fillMaxWidth(0.85f),
+            modifier = Mdf.fillMaxWidth(0.85f).clip(RoundedCornerShape(10.dp)),
             content = {
                 items(count = records.size) { index: Int ->
                     HomeCard(homeRecordCardUI = records[index], modifier = Mdf.fillMaxWidth())
