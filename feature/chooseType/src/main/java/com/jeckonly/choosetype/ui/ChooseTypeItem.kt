bd4933b7cfffdaff6e7bb49f6a36228e08626443
JeckOnly <2322802221@qq.com>
2023-01-02 11:58:01 +0800

    文字对齐

--- feature/chooseType/src/main/java/com/jeckonly/choosetype/ui/ChooseTypeItem.kt
+++ feature/chooseType/src/main/java/com/jeckonly/choosetype/ui/ChooseTypeItem.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.jeckonly.core_model.entity.helper.ExpenseOrIncome
@@ -60,7 +61,8 @@ fun ChooseTypeItem(
             text = typeUI.typeName,
             style = MaterialTheme.typography.bodySmall,
             color = contentColorFor(backgroundColor = MaterialTheme.colorScheme.surface),
-            modifier = Mdf.padding(top = 8.dp)
+            modifier = Mdf.padding(top = 8.dp),
+            textAlign = TextAlign.Center
         )
     }
 }
